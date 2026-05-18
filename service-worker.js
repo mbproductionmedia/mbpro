2937b092303bef3be2283a68603c28ed8b5dd534
mbproductionmedia <mbproduction.media@gmail.com>
2026-05-19 01:09:58 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'mbpro-v1';
+const ASSETS = [
+  '/mbpro/',
+  '/mbpro/index.html',
+  '/mbpro/manifest.json',
+  '/mbpro/icon-192.png',
+  '/mbpro/icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('/mbpro/index.html');
+      });
+    })
+  );
+});
